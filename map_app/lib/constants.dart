2b154ca8087c9a1dc63b48d5e6d2aab8db0c779b
Rajin Hossain <rajinsuperstar3@gmail.com>
2023-08-15 11:21:30 +1000

    initial map integration

---
+++ map_app/lib/constants.dart
@@ -0,0 +1,7 @@
+class AppLevelConstants {
+  static const String mbAccessToken =
+      "pk.eyJ1IjoicmFqaW5ob3NzYWluIiwiYSI6ImNsbGJraDNxcDAybHAzbnE1cDAza29kbDcifQ.52uNb3R4BKvyjv6zbduyRA";
+  static const String mbStyleID = "cllbkoxl000dj01pw6xnm42kb";
+  static const String mbShareURL =
+      "https://api.mapbox.com/styles/v1/rajinhossain/$mbStyleID/tiles/256/{z}/{x}/{y}@2x?access_token=$mbAccessToken";
+}
--- map_app/lib/main.dart
+++ map_app/lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_map/plugin_api.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:latlong2/latlong.dart';
+import 'package:map_app/constants.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,10 +13,44 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    ThemeData buildTheme(brightness) {
+      var baseTheme = ThemeData(brightness: brightness);
+      return baseTheme.copyWith(
+        textTheme: GoogleFonts.poppinsTextTheme(baseTheme.textTheme),
+      );
+    }
+
+    return MaterialApp(
+      theme: buildTheme(Brightness.light),
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        appBar: AppBar(
+          backgroundColor: Colors.blueAccent,
+          elevation: 0,
+          title: const Text(
+            'Maps Integration',
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+        ),
+        body: Stack(
+          children: [
+            FlutterMap(
+              options: MapOptions(
+                minZoom: 5,
+                maxZoom: 18,
+                zoom: 12,
+                center: const LatLng(-35.282053, 149.128670),
+              ),
+              children: [
+                TileLayer(
+                  urlTemplate: AppLevelConstants.mbShareURL,
+                  additionalOptions: const {
+                    'mapStyleId': AppLevelConstants.mbStyleID,
+                    'accessToken': AppLevelConstants.mbAccessToken
+                  },
+                )
+              ],
+            )
+          ],
         ),
       ),
     );
